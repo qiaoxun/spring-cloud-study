80f00a4475193124af7c6f4da310e9234c20db2f
Xun Qiao <woqiaoxun@163.com>
2019-10-11 17:08:33 +0800

    first commit

---
+++ src/main/java/com/study/App.java
@@ -0,0 +1,21 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class);
+    }
+}
