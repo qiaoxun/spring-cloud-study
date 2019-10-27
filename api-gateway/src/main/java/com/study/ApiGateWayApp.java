20c20e7c3aa847adea12327f1f426d2c4c42c496
Joey <woqiaoxun@163.com>
2019-10-27 19:07:25 +0800

    add zuul

---
+++ api-gateway/src/main/java/com/study/ApiGateWayApp.java
@@ -0,0 +1,17 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGateWayApp {
+    public static void main( String[] args ) {
+        SpringApplication.run(ApiGateWayApp.class);
+    }
+}
