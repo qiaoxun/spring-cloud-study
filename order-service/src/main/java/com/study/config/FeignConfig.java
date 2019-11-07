2ce47c7ac37367ae9e1b05c2a49480a4582f050c
Joey <woqiaoxun@163.com>
2019-11-07 23:17:34 +0800

    study

--- order-service/src/main/java/com/study/config/FeignConfig.java
+++ order-service/src/main/java/com/study/config/FeignConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignConfig {
 
-    @Bean
+//    @Bean
     Retryer feignRetryer() {
         return Retryer.NEVER_RETRY;
     }
