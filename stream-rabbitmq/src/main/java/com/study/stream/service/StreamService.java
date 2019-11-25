e74d7210194e13bebefa4b3cc1adf17f620f9063
Joey <woqiaoxun@163.com>
2019-11-25 23:59:23 +0800

    remove rabbitmq template

--- stream-rabbitmq/src/main/java/com/study/stream/service/StreamService.java
+++ stream-rabbitmq/src/main/java/com/study/stream/service/StreamService.java
@@ -1,7 +1,6 @@
 package com.study.stream.service;
 
 import com.study.stream.channel.SpringStreamChannel;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -9,9 +8,6 @@ import org.springframework.messaging.support.MessageBuilder;
 
 @EnableBinding(SpringStreamChannel.class)
 public class StreamService {
-
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
 
     @Autowired
     private SpringStreamChannel channel;
